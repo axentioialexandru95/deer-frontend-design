0146db8b68a11ed01ce8a2d1583d2f629e0b7535
Axentioi Alexandru <axentioialexandru95@gmail.com>
2017-07-17 16:25:02 +0300

    Deer frontend upload

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Deer</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="css/main.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
+    <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+  <body id="top">
+    <div class="site" data-aos="fade" data-aos-duration="2000" >
+    <!-- Nav -->
+    <div class="nav">
+      <div class="logo">
+        <h1>CE SOIR</h1>
+        <hr>
+      </div>
+      <div class="right">
+        <ul>
+          <li><a href="#story">Our Story</a> <hr></li>
+          <li><a href="#fare">Fare</a><hr></li>
+          <li><a href="#drive">Drive</a><hr></li>
+          <li><a href="#contact">Contact</a><hr></li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- Header -->
+    <div class="header text-center" >
+      <div class="background"></div>
+      <div class="logo text-center">
+        <img src="images/logo.png" alt="">
+      </div>
+      <div class="text-center">
+        <a href="#story"><i class="ion-ios-arrow-down arrow-down"></i></a>
+      </div>
+    </div>
+
+    <!-- Story -->
+    <div class="story" id="story" data-aos="fade-up">
+      <div class="left text-center">
+        <div class="background"></div>
+        <div class="text">
+          <h1>Our hello</h1>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h3><a href="#fare">Fresh Fare</a></h3>
+        </div>
+      </div>
+
+      <div class="right">
+      </div>
+    </div>
+
+    <!-- Fresh Fare -->
+    <div class="fare text-center" id="fare" data-aos="fade-up">
+      <div class="left"></div>
+      <div class="right">
+        <div class="background"></div>
+        <div class="text">
+          <img src="images/fare.png" alt="Fresh Fare">
+          <h1>Fresh Fare</h1>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          <a href="#drive">ALL NATURAL</a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Drive -->
+    <div class="drive" id="drive" data-aos="fade-up">
+      <div class="left text-center">
+        <div class="background"></div>
+        <div class="text">
+          <img src="images/seeds.png" alt="Fresh Fare">
+          <h1>Our hello</h1>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h3><a href="#contact">Sodium-free</a></h3>
+        </div>
+      </div>
+
+      <div class="right">
+        <div class="background"></div>
+      </div>
+    </div>
+
+    <!-- Parallax -->
+    <div class="parallax"></div>
+
+    <!-- Contact -->
+    <div class="contact" >
+      <div class="background"></div>
+      <div class="left">
+        <div class="logo">
+          <h1>CE SOIR</h1>
+          <hr>
+        </div>
+        <div class="text">
+          <h4>Phone Number: 0751 683 911</h4>
+          <h4>Email: axentioialexandru95@gmail.com</h4>
+          <div class="alternative">
+            <p>Some text here if you suppose you want to..</p>
+            <p>Some text here if you suppose you want to..</p>
+          </div>
+        </div>
+      </div>
+      <div class="right">
+        <div class="social">
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-instagram"></i></a>
+          <a href="#"><i class="fa fa-google-plus"></i></a>
+        </div>
+      </div>
+      <a href="#top" id="contact"><i class="ion-ios-arrow-up arrow-up"></i></a>
+    </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
